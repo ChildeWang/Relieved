40b50ed335508955dfb02518c60883ca634cffce
Gentle <494605779@qq.com>
2015-09-21 09:50:31 +0800

    First day.

---
+++ Travel/Travel/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Travel
+//
+//  Created by Gentle on 15/9/21.
+//  Copyright (c) 2015年 Gentle. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Travel/Travel/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Travel
+//
+//  Created by Gentle on 15/9/21.
+//  Copyright (c) 2015年 Gentle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
